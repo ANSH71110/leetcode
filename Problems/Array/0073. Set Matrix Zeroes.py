23c5a7d643d9f553a6c67972850a9394390a7692
ANSH71110 <79516113+ANSH71110@users.noreply.github.com>
2022-05-03 20:03:51 +0530

    Update 0073. Set Matrix Zeroes.py
    optimal solution but O(n) space comp

--- Problems/Array/0073. Set Matrix Zeroes.py
+++ Problems/Array/0073. Set Matrix Zeroes.py
@@ -7,37 +7,21 @@ class Solution:
         n=len(matrix[0])
         i=0
         j=0
-        riz=0
-        rjz=0
+        r=set()
+        c=set()
         while i<m:
             j=0
             while j<n:
                 if matrix[i][j]==0:
-                    matrix[0][j]=0
-                    matrix[i][0]=0
-                    if i==0:
-                        riz=1
-                    if j==0:
-                        rjz=1
+                    r.add(i)
+                    c.add(j)
                 j+=1
             i+=1
-        i=1
+        i=0
         while i<m:
-            j=1
+            j=0
             while j<n:
-                if matrix[0][j]==0 or matrix[i][0]==0:
+                if i in r or j in c:
                     matrix[i][j]=0
                 j+=1
             i+=1
-        if riz==1:
-            i=0
-            j=0
-            while j<n:
-                matrix[i][j]=0
-                j+=1
-        if rjz==1:
-            i=0
-            j=0
-            while i<m:
-                matrix[i][j]=0
-                i+=1
