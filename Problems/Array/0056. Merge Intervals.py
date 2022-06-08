b87f805eef50d258b6de44c8604ac590e4c3f6a1
ANSH71110 <79516113+ANSH71110@users.noreply.github.com>
2022-06-09 00:36:01 +0530

    Create 0056. Merge Intervals.py
    naive

---
+++ Problems/Array/0056. Merge Intervals.py
@@ -0,0 +1,12 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        i=0
+        intervals.sort()
+        while i<len(intervals)-1:
+            if intervals[i][-1]>=intervals[i+1][0]:
+                intervals[i][0]=min(intervals[i][0],intervals[i+1][0])
+                intervals[i][-1]=max(intervals[i+1][-1],intervals[i][-1])
+                intervals.pop(i+1)
+                i-=1
+            i+=1
+        return intervals
