3b833f87cb24ce1ca58585f458d2fb7bfb005ba6
ANSH71110 <79516113+ANSH71110@users.noreply.github.com>
2022-06-09 17:19:25 +0530

    Update 0167. Two Sum II - Input Array Is Sorted.py
    O(1) space comp

--- Problems/Array/0167. Two Sum II - Input Array Is Sorted.py
+++ Problems/Array/0167. Two Sum II - Input Array Is Sorted.py
@@ -1,16 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        i=0
-        l=[]
-        j=len(nums)-1
-        while i<j:
-            #print(nums[i],nums[j],target,i,j)
-            if nums[i]+nums[j]>target:
-                j-=1
-            elif nums[i]+nums[j]<target:
-                i+=1
+        x=0
+        y=len(nums)-1
+        while x<y:
+            if nums[x]+nums[y]==target:
+                return [x+1,y+1]
+            elif nums[x]+nums[y]>target:
+                y-=1
             else:
-                l.append(i+1)
-                l.append(j+1)
-                return l
-        return l
+                x+=1
