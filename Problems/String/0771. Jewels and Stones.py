f316fc26274e6aff2b1e094763b70d0080168047
ANSH71110 <79516113+ANSH71110@users.noreply.github.com>
2022-06-10 17:15:08 +0530

    Update 0771. Jewels and Stones.py
    O(1) space

--- Problems/String/0771. Jewels and Stones.py
+++ Problems/String/0771. Jewels and Stones.py
@@ -3,11 +3,6 @@ class Solution:
         i=0
         count=0
         while i<len(j):
-            c=j[i]
-            k=0
-            while k<len(s):
-                if c==s[k]:
-                    count+=1
-                k+=1
+            count+=s.count(j[i])
             i+=1
         return count
