7133eadd3ec386dc7ad7b4b19a3457af77456501
ANSH71110 <79516113+ANSH71110@users.noreply.github.com>
2022-06-10 16:01:06 +0530

    Update 2259. Remove Digit From Number to Maximize Result.py
    optimal sol

--- Problems/String/2259. Remove Digit From Number to Maximize Result.py
+++ Problems/String/2259. Remove Digit From Number to Maximize Result.py
@@ -2,11 +2,11 @@ class Solution:
     def removeDigit(self, number: str, digit: str) -> str:
         l=list(number)
         i=0
-        g=[]
+        g=0
         while i<len(l):
             if l[i]==digit:
                 l.pop(i)
-                g.append(int("".join(l)))
+                g=max(g,int("".join(l)))
                 l=list(number)
             i+=1
-        return str(max(g))
+        return str(g)
