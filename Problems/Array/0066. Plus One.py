e6312fc4d6d30f7dd7d047ab3a9128d710c9ea99
ANSH71110 <79516113+ANSH71110@users.noreply.github.com>
2022-06-17 16:58:14 +0530

    Delete 0066. Plus One.py

--- Problems/Array/0066. Plus One.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def plusOne(self, d: List[int]) -> List[int]:
-        i=0
-        n=0
-        while i<len(d):
-            n*=10
-            n+=d[i]            
-            i+=1
-        return [int(i) for i in str(n+1)]
