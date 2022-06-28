4832b2f73d3083497f370d6b90427312bdd0b8ad
ANSH71110 <79516113+ANSH71110@users.noreply.github.com>
2022-06-29 01:49:37 +0530

    Create 1647. Minimum Deletions to Make Character Frequencies Unique.py
    memory efficient

---
+++ Problems/String/1647. Minimum Deletions to Make Character Frequencies Unique.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minDeletions(self, s: str) -> int:
+        l=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        for i in s:
+            l[ord(i)-97]+=1
+        s=sum(l)
+        k=0
+        l.sort(reverse=True)
+        while k<len(l):
+            j=0
+            while j<len(l):
+                if l[k]==l[j] and l[k]>0 and k!=j:
+                    l[k]-=1
+                    j=-1
+                elif l[k]==0:
+                    l.pop(k)
+                    k-=1
+                    break
+                j+=1
+            k+=1
+            
+        return s-sum(l)
