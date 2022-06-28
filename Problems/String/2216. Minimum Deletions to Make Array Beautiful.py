66e444623f56c1e79a4959e43e1930ff6437264f
ANSH71110 <79516113+ANSH71110@users.noreply.github.com>
2022-06-29 02:36:21 +0530

    Delete 2216. Minimum Deletions to Make Array Beautiful.py

--- Problems/String/2216. Minimum Deletions to Make Array Beautiful.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def minDeletion(self, nums: List[int]) -> int:
-        i,s=0,0
-        while i<len(nums)-1:
-            if (i-s)%2==0 and nums[i]==nums[i+1]:
-                s+=1
-            i+=1
-        return s+(len(nums)-s) %2
