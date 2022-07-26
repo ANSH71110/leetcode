f64f197914a6bbf94e1bd61593c61bd926dcfce1
ANSH71110 <79516113+ANSH71110@users.noreply.github.com>
2022-07-26 22:18:20 +0530

    Create 0897. Increasing Order Search Tree.py
    Using Stack- O(n) runtime and space

---
+++ Problems/Tree/0897. Increasing Order Search Tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        stack=[]
+        dummy=curr=TreeNode()
+        while stack or root:
+            while root:
+                stack.append(root)
+                root=root.left
+            
+            root=stack.pop()
+            curr.right=root
+            
+            curr=curr.right
+            root=root.right
+            
+            curr.left=None
+        return dummy.right
