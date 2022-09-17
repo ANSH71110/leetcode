3c7b74ccddb6f96f1901c5c8099eb906b16c2a71
ANSH CHANDARANA <79516113+ANSH71110@users.noreply.github.com>
2022-09-18 02:45:19 +0530

    Update 0496. Next Greater Element I.py
    O(n) - time and space comp
    runtime dec and code is simple

--- Problems/Array/0496. Next Greater Element I.py
+++ Problems/Array/0496. Next Greater Element I.py
@@ -1,18 +1,11 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        mapp={}
         stack=[]
-        ans=[]
+        mapp={}
         
         for i in nums2:
             while stack and i>stack[-1]:
                 mapp[stack.pop()]=i
             stack.append(i)
         
-        while stack:
-            mapp[stack.pop()]=-1
-        
-        for i in nums1:
-            ans.append(mapp[i])
-        
-        return ans
+        return [mapp.get(i,-1) for i in nums1]
