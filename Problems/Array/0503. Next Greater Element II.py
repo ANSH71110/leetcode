2a2fc4c090049849fefb0507b7939f6232914374
ANSH CHANDARANA <79516113+ANSH71110@users.noreply.github.com>
2022-09-18 02:37:56 +0530

    Create 0503. Next Greater Element II.py
    circular array 2 iterations are sufficient fir next greatest element.
    O(n) - tame and space

---
+++ Problems/Array/0503. Next Greater Element II.py
@@ -0,0 +1,17 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        stack=[]
+        ans=[-1]*len(nums)
+        
+        for i in range(len(nums)):
+            while stack and nums[stack[-1]]<nums[i]:
+                ans[stack.pop()]=nums[i]
+            stack.append(i)
+        
+        for i in range(len(nums)):
+            while stack and nums[stack[-1]]<nums[i]:
+                ans[stack.pop()]=nums[i]
+            if stack==[]:
+                break
+        
+        return ans
